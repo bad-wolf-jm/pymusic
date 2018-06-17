b700fee8f0eebf9db7e1b31844d16591f6a7a71e
Jean-Martin <jm.albert@gmail.com>
2018-06-16 21:37:19 -0400

    removed semantic-ui

---
+++ electron/web-audio/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var electron = require('electron');
+
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+      width: 1728,
+      height: 1152,
+    });
+    //mainWindow.setMenu(null);
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // mainWindow.loadURL('file://' + __dirname + '/dbedit_ui/main_ui_edit.html');
+});
