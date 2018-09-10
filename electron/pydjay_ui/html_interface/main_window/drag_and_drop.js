d50684a44dd721ffbf6af8a2ec354097ede4b3fc
Jean-Martin <jm.albert@gmail.com>
2018-09-10 08:59:46 -0400

    Drag and drop

--- electron/pydjay_ui/html_interface/main_window/drag_and_drop.js
+++ electron/pydjay_ui/html_interface/main_window/drag_and_drop.js
@@ -2,14 +2,16 @@ var accordion_open_promise = undefined
 
 function open_accordion(evt) {
     let panel = evt.target.nextElementSibling;
-    accordion_open_promise = setInterval(() => {
+    //console.log(evt)
+    accordion_open_promise = setTimeout(() => {
         evt.target.classList.toggle("active");
         panel.style.maxHeight = panel.scrollHeight + "px";
+        //clearInterval(accordion_open_promise)
     }, 1000)
 }
 
 function cancel_open_accordion() {
-    clearInterval(accordion_open_promise)
+    clearTimeout(accordion_open_promise)
 }
 
 var droppable = document.querySelectorAll('.open-on-drag-over');
--- electron/pydjay_ui/html_interface/main_window/views/playlists_view.js
+++ electron/pydjay_ui/html_interface/main_window/views/playlists_view.js
@@ -14,6 +14,36 @@ class PlaylistsView extends EventDispatcher {
         this.controller.ready(this.set_list.bind(this))
     }
 
+    on_drag_enter(evt) {
+        evt.target.parentElement.classList.add("drop-here")
+    }
+
+    on_drag_leave(evt) {
+        evt.target.parentElement.classList.remove("drop-here")
+    }
+
+    on_drag_over(evt) {
+        if (evt.preventDefault) {
+            evt.preventDefault();
+          }
+          evt.dataTransfer.dropEffect = 'move';
+    }
+
+    on_drag_end(evt) {
+        console.log(evt.target)
+    }
+
+    on_drop(evt) {
+        if (evt.stopPropagation) {
+            evt.stopPropagation();
+          }
+        console.log(evt.target.parentElement)
+        evt.target.parentElement.classList.remove("drop-here")
+        let d = evt.dataTransfer.getData("text/plain")
+        console.log(JSON.parse(d))
+  }
+
+
     set_list(list) {
         this.view_list_order = []
         let list_rows = []  
@@ -34,5 +64,15 @@ class PlaylistsView extends EventDispatcher {
                 });
             }
         )
+        var drop_targets = document.querySelectorAll('.track-drop-target');
+        [].forEach.call(drop_targets, (col) => {
+            //console.log(col)
+            col.addEventListener('dragenter', this.on_drag_enter.bind(this), false);
+            col.addEventListener('dragleave', this.on_drag_leave.bind(this), false);
+            col.addEventListener('dragover',  this.on_drag_over.bind(this), false);
+            col.addEventListener('dragend',   this.on_drag_end.bind(this), false);
+            col.addEventListener('drop',      this.on_drop.bind(this), false);
+        });
+
     }
 }
