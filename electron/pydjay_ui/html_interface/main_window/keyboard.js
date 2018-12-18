f1f5c7c8a0e1b6607dcfdda714eeddc42a69fd6a
J-M <jm.albert@gmail.com>
2018-12-18 12:45:31 -0500

    Added mousetrap library for keyboard events

---
+++ electron/pydjay_ui/html_interface/main_window/keyboard.js
@@ -0,0 +1,32 @@
+
+//var keyboard_listener = Mousetrap()
+
+
+Mousetrap.bind('esc', () => {
+    console.log('escape');
+});
+
+
+Mousetrap.bind('up', () => {
+    console.log('up');
+});
+
+Mousetrap.bind('down', () => {
+    console.log('down');
+});
+
+Mousetrap.bind('left', () => {
+    console.log('left');
+});
+
+Mousetrap.bind('right', () => {
+    console.log('right');
+});
+
+Mousetrap.bind('home', () => {
+    console.log('home');
+});
+
+
+
+Mousetrap.bind('g i', function() { console.log('go to inbox'); });
