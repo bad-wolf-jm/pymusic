fc76b406e7205acf2bdcd79007654553c271557d
J-M <jm.albert@gmail.com>
2018-12-26 20:43:56 -0500

    Removed empty file

--- electron/pydjay_ui/html_interface/main_window/cluster.js
+++
@@ -1,9 +0,0 @@
-
-
-// // JavaScript
-// var data = ['<tr>…</tr>', '<tr>…</tr>', …];
-// var clusterize = new Clusterize({
-//   rows: data,
-//   scrollId: 'scrollArea',
-//   contentId: 'contentArea'
-// });
