438be3743b26b7f7c1308c986ab3c30ba6cf9b5f
J-M <jm.albert@gmail.com>
2018-12-29 00:00:00 -0500

    Model operations

--- electron/pydjay_ui/models/base/ordered_subset_model.js
+++ electron/pydjay_ui/models/base/ordered_subset_model.js
@@ -7,4 +7,31 @@ class BaseOrderedObjectSubsetModel extends BaseObjectSubsetModel {
     get_all_objects() {
         return this.model.get_objects(this.ordering)
     }
+
+    reorder(new_order) {
+        if (new_order.length == this.ordering.length) {
+            if (new_order.every((x) => {this.objects[x] != undefined})) {
+                this.ordering = new_order
+                this.dispatch("reorder", this.get_all_objects())
+            }
+        }
+    }
+
+    insert(element, index) {
+        this.ordering.splice(index, 0, element.id)
+        this.add(element)
+    }
+
+    append(element) {
+        this.ordering.push(element.id)
+        this.add(element)
+    }
+
+    remove(element) {
+        let index = this.ordering.indexOf(element.id)
+        if (index != -1) {
+            this.ordering.splice(I, 1)
+            super.remove(element)
+        }
+    }
 }
